27f0572d20973db06e7c2a5afcb71c64fcf9d323
jiangxianping <992386253@qq.com>
2019-10-28 17:29:19 +0800

    Initial commit

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/jiang/DeptProvider8001_App.java
@@ -0,0 +1,12 @@
+package com.jiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8001_App {
+
+    public static void main(String[] args){
+        SpringApplication.run(DeptProvider8001_App.class);
+    }
+}
