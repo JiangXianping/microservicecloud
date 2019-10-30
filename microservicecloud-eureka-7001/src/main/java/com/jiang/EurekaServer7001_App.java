2420d0461f4bf13a9ce8328f316cdf958f3de7f5
jiangxianping <992386253@qq.com>
2019-10-30 17:34:24 +0800

    Ribbon负载均衡初步搭建

--- microservicecloud-eureka-7001/src/main/java/com/jiang/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/jiang/EurekaServer7001_App.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer//EurekaServer服务器端启动类，接受其它微服务注册进来
 public class EurekaServer7001_App {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer7001_App.class,args);
-}
+        SpringApplication.run(EurekaServer7001_App.class, args);
+    }
 }
--- microservicecloud-provider-dept-8004/src/main/java/com/jiang/DeptProvider8003_App.java
+++ microservicecloud-provider-dept-8004/src/main/java/com/jiang/DeptProvider8003_App.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class DeptProvider8001_App {
+public class DeptProvider8003_App {
 
     public static void main(String[] args){
-        SpringApplication.run(DeptProvider8001_App.class,args);
+        SpringApplication.run(DeptProvider8003_App.class,args);
     }
 }
